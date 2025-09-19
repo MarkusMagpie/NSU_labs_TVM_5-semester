e4e38642b7aa889c98ef5888d10f491e361a6b75
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-09-19 21:21:49 +0700

    готова lab05

--- lab05/src/compiler.ts
+++ lab05/src/compiler.ts
@@ -4,7 +4,33 @@ import { buildOneFunctionModule, Fn } from "./emitHelper";
 const { i32, get_local} = C;
     
 export function getVariables(e: Expr): string[] {
-    throw "Not implemented";
+    // throw "Not implemented";
+    let variables: string[] = [];
+
+    function colectVars(e: Expr) {
+        switch(e.type) {
+            case 'num':
+                // в числах переменных нема
+                break;
+            case 'var':
+                // если ещё нет в списке -> добавляю
+                if (!variables.includes(e.name)) {
+                    variables.push(e.name);
+                }
+                break;
+            case 'neg':
+                colectVars(e.arg);
+                break;
+            case 'bin':
+                colectVars(e.left);
+                colectVars(e.right);
+                break;
+        }
+    }
+
+    colectVars(e);
+
+    return variables;
 }
 
 export async function buildFunction(e: Expr, variables: string[]): Promise<Fn<number>>
@@ -14,5 +40,25 @@ export async function buildFunction(e: Expr, variables: string[]): Promise<Fn<nu
 }
 
 function wasm(e: Expr, args: string[]): Op<I32> {
-    throw "Not implemented";
+    // throw "Not implemented";
+    switch (e.type) {
+        case 'num':
+            return i32.const(e.value);
+        case 'var':
+            const index = args.indexOf(e.name);
+            if (index === -1) {
+                throw new Error();
+            }
+
+            return get_local(i32, index);
+        case 'neg':
+            return i32.mul(i32.const(-1), wasm(e.arg, args));
+        case 'bin':
+            switch (e.operation) {
+                case '+': return i32.add(wasm(e.left, args), wasm(e.right, args));
+                case '-': return i32.sub(wasm(e.left, args), wasm(e.right, args));
+                case '*': return i32.mul(wasm(e.left, args), wasm(e.right, args));
+                case '/': return i32.div_s(wasm(e.left, args), wasm(e.right, args));
+            }
+    }
 }
