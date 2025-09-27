d577855f8e806b73b83d42bf9b122b541c16cae9
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-09-27 18:01:33 +0700

    сделана функция cost()

--- lab07/src/simplify.ts
+++ lab07/src/simplify.ts
@@ -3,4 +3,17 @@ import { Expr } from "../../lab04";
 export function simplify(e: Expr, identities: [Expr, Expr][]): Expr
 {
     return e;
+    // let result: Expr;
+    // switch(e.type) {
+    //     case "num":
+
+    //     case "var":
+
+    //     case "neg":
+
+    //     case "bin":
+            
+    // }
+
+    // return result;
 }
